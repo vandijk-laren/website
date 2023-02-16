bb041022d525c73d2c0184810eacd7fdde1cd4c4
Luuk van Dijk <Luukvandijk2003@gmail.com>
2023-02-16 14:56:34 +0100

    navbar basic styling

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,8 +1,18 @@
 import React from 'react'
 import styles from "./navbar.module.css"
+import logoBlue from "../../assets/logoBlue.svg"
 
 export default function Navbar() {
     return (
-        <div>Navbar</div>
+        <div className={styles.mainContainer}>
+            <img id={styles.img} src={logoBlue} alt='logo van van dijk laren' />
+            <div className={styles.navContainer}>
+                <p id={styles.navText}>Honden</p>
+                <p id={styles.navText}>Katten</p>
+                <p id={styles.navText}>en meer</p>
+                <div className={styles.devider} />
+                <p id={styles.navText}>Contact</p>
+            </div>
+        </div>
     )
 }
