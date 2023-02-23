2b1d23b401eb1fd11527e70a291feafd299e2367
Luuk van Dijk <luukvandijk2003@gmail.com>
2023-02-23 13:37:48 +0100

    added folders

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import HomePage from "./pages/HomePage"
+import HomePage from "./pages/homePage/HomePage"
 
 function App() {
   return (
---
+++ src/pages/homePage/HomePage.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./homePage.css"
+import Navbar from "../../components/navbar/Navbar"
+import Landing from "../../components/landing/Landing"
+import NieuweProducten from "../../components/nieuweProducten/NieuweProducten"
+import OverDeWinkel from "../../components/overDeWinkel/OverDeWinkel"
+import Footer from "../../components/footer/Footer"
+
+export default function HomePage() {
+    return (
+        <div className='mainContainer'>
+            <Navbar />
+            <Landing />
+            <NieuweProducten />
+            <OverDeWinkel />
+            <Footer />
+        </div>
+    )
+}
