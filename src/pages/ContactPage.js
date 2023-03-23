f96b10886799b73087e7c0d27af324766c5b09b7
Juicy_Jesse <91599008+JesseElbertsen@users.noreply.github.com>
2023-03-23 14:05:42 +0100

    ContactFormV1.0

---
+++ src/pages/ContactPage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "../components/navbar/Navbar";
+import Landing from "../components/landing/Landing";
+import Footer from "../components/footer/Footer";
+import Contact from "../components/Contact/Contact";
+
+export default function ContactPage() {
+  return (
+    <div>
+      <Navbar />
+      <Landing />
+      <Contact />
+      <Footer />
+    </div>
+  );
+}
